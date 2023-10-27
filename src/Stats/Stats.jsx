2cf7ae38f6c4bfd4c0995a9fdcdf60770fb2d62f
David <Rigby335@hotmail.com>
2023-10-26 21:16:00 -0700

    ft. displaying mapped array of stats objs

--- src/Stats/Stats.jsx
+++ src/Stats/Stats.jsx
@@ -1,12 +1,20 @@
-import { statsObj } from "../stats";
+import { headerIconsObj, statsObj } from "../stats";
 import "./Stats.css";
 
 export default function Stats() {
   console.log(statsObj);
+  console.log("headerIconsObj", headerIconsObj);
   return (
     <div className="stats-container">
       <header>
         <h3>Skills</h3>
+        {statsObj.map((obj) => {
+          return (
+            <section key={obj.id} className="skill-row">
+              #{obj.id} {obj.name} {obj.rating}
+            </section>
+          );
+        })}
         <section className="header-icons">
           <a href=""></a>
           <a href=""></a>
