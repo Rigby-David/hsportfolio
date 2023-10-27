edddb77d97b38899c3d563681be629c892c37f20
David <Rigby335@hotmail.com>
2023-10-26 21:11:41 -0700

    ft. reshaped data in stats.js

--- src/stats.js
+++ src/stats.js
@@ -1,68 +1,92 @@
-const statsObj = {
-  skills: [
-    {
-      id: 1,
-      name: "React",
-      rating: "99.9%",
-    },
-    {
-      id: 2,
-      name: "JavaScript",
-      rating: "99.9%",
-    },
-    {
-      id: 3,
-      name: "HTML",
-      rating: "99.9%",
-    },
-    {
-      id: 4,
-      name: "CSS",
-      rating: "99.9%",
-    },
-    {
-      id: 5,
-      name: "Postgresql",
-      rating: "99.9%",
-    },
-    {
-      id: 6,
-      name: "TDD",
-      rating: "99.9%",
-    },
-    {
-      id: 7,
-      name: "Netlify",
-      rating: "99.9%",
-    },
-    {
-      id: 8,
-      name: "Node.js",
-      rating: "99.9%",
-    },
-    {
-      id: 9,
-      name: "TypeScript",
-      rating: "99.9%",
-    },
-    {
-      id: 10,
-      name: "Supabase",
-      rating: "99.9%",
-    },
-    {
-      id: 11,
-      name: "Express",
-      rating: "99.9%",
-    },
-    // {
-    //   id: 12,
-    //   name: "GitHub",
-    //   rating: "99.9%",
-    // },
-  ],
-};
+const statsObj = [
+  {
+    id: 1,
+    name: "React",
+    rating: "99.9%",
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+    rating: "99.9%",
+  },
+  {
+    id: 3,
+    name: "HTML",
+    rating: "99.9%",
+  },
+  {
+    id: 4,
+    name: "CSS",
+    rating: "99.9%",
+  },
+  {
+    id: 5,
+    name: "Postgresql",
+    rating: "99.9%",
+  },
+  {
+    id: 6,
+    name: "TDD",
+    rating: "99.9%",
+  },
+  {
+    id: 7,
+    name: "Netlify",
+    rating: "99.9%",
+  },
+  {
+    id: 8,
+    name: "Node.js",
+    rating: "99.9%",
+  },
+  {
+    id: 9,
+    name: "TypeScript",
+    rating: "99.9%",
+  },
+  {
+    id: 10,
+    name: "Supabase",
+    rating: "99.9%",
+  },
+  {
+    id: 11,
+    name: "Express",
+    rating: "99.9%",
+  },
+];
 
-const headerIconsObj = {};
+const headerIconsObj = [
+  {
+    id: 1,
+    name: "home",
+    href: "",
+    icon: "",
+  },
+  {
+    id: 2,
+    name: "about",
+    href: "",
+    icon: "",
+  },
+  {
+    id: 3,
+    name: "contact",
+    href: "",
+    icon: "",
+  },
+  {
+    id: 4,
+    name: "resume",
+    href: "",
+    icon: "",
+  },
+  {
+    id: 5,
+    name: "projects",
+    href: "",
+    icon: "",
+  },
+];
 
-export { statsObj };
+export { statsObj, headerIconsObj };
